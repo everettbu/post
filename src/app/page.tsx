165686036e37a786d7b26a6d212a72d94552606d
Everett <your-github-email@example.com>
2025-01-23 17:23:05 -0800

    bun

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-'use client';
-
-import React, { useEffect, useState } from "react";
-
+import React from "react";
 export default function Home() {
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  if (!isMounted) {
-    return null;
-  }
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
