41ebee8fb71f6ee4c0e92ffa5c6389f890594349
lizard-boy <your-github-email@example.com>
2025-01-30 16:58:46 -0800

    photos uploaded

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -15,29 +15,43 @@ export const initialPhotos: Photo[] = [
   {
     id: '1',
     url: '/photos/matthew.jpg',
-    caption: 'Not here',
-    date: '09-14-2001',
+    caption: 'Met Matthew behind a 7-11',
+    date: '05-08-2018',
     likes: 0
   },    
   {
     id: '2',
-    url: '/photos/matthew.jpg',
-    caption: 'HERE',
-    date: '2024-03-20',
+    url: '/photos/carp.jpg',
+    caption: 'Holy Carp',
+    date: '07-12-2015',
     likes: 0
   },
   {
     id: '3',
-    url: '/photos/matthew.jpg',
-    caption: '',
-    date: '2024-03-20',
+    url: '/photos/snowie.jpg',
+    caption: 'Dawg',
+    date: '07-12-2015',
     likes: 0
   },
   {
     id: '4',
-    url: '/photos/matthew.jpg',
-    caption: 'Cheese',
-    date: '2024-01-20',
+    url: '/photos/kiss.jpg',
+    caption: 'Big fish, big kiss',
+    date: '08-23-2024',
+    likes: 0
+  },
+  {
+    id: '5',
+    url: '/photos/peru.jpg',
+    caption: 'Astonomer ruins in peru',
+    date: '01-10-2024',
+    likes: 0
+  },
+  {
+    id: '6',
+    url: '/photos/buddy.jpg',
+    caption: 'Met HEB Buddy',
+    date: '12-21-2017',
     likes: 0
   }
 ];
