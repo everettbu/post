32e943bfd55c3b85a6d0fb241c22bfd733cfeef6
everettbu <everettbutler@rocketmail.com>
2025-09-05 17:09:40 -0700

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <h1>Hello World</h1>
+      <h1 className="text-6xl font-bold">I am a fat fart</h1>
     </div>
   );
 }
